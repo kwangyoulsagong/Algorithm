d7c02638d99faa44c27bcd5a7b448b5543905a40
Tkrhdrhkdduf <61825101+Tkrhdrhkdduf@users.noreply.github.com>
2022-04-04 22:31:15 +0900

    Create 2739 구구단.c

---
+++ 백준/2739 구구단.c
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int N;
+  int result;
+  for (int i=0; i<N; i++){
+    result=N*i;
+    cout<<N<<" * "<<i<<" = "<<result<<" "<<endl;
+  }
+}
