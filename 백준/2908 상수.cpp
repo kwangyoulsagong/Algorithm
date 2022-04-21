f4ecdc67e971579d7425f69da4880721031f03e1
Tkrhdrhkdduf <61825101+Tkrhdrhkdduf@users.noreply.github.com>
2022-04-21 21:01:01 +0900

    Update 2908 상수.cpp

--- 백준/2908 상수.cpp
+++ 백준/2908 상수.cpp
@@ -14,4 +14,24 @@ int main() {
 
 
 }
+#include <iostream>
+using namespace std;
+int main() {
+	string a, b;
+	cin >> a >> b;
+	string bigger;
+	for (int i = 2; i >= 0; i--) {
+		if (a[i] > b[i]) {
+			bigger = a;
+			break;
+		}
+		else if(a[i] < b[i]) {
+			bigger = b;
+			break;
+		}
+	}
+	cout << bigger[2]<<bigger[1]<<bigger[0];
+
+}
+
 
