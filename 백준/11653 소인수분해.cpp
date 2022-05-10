69dbec2277256ae53dbb6cdb07dcf75a06cb8d5d
Tkrhdrhkdduf <61825101+Tkrhdrhkdduf@users.noreply.github.com>
2022-05-10 23:06:17 +0900

    Update 11653 소인수분해.cpp

--- 백준/11653 소인수분해.cpp
+++ 백준/11653 소인수분해.cpp
@@ -14,3 +14,18 @@ int main() {
 	}
 
 }
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >>n;
+
+	for (int i = 2; i <= n; i++) {
+		while (n % i == 0) {
+			cout << i << "\n";
+			n /= i;
+		}
+	
+	}
+
+}
