58cdc1cb991dfbd45e271e70b427e207853ecdd0
Tkrhdrhkdduf <61825101+Tkrhdrhkdduf@users.noreply.github.com>
2022-10-03 17:56:52 +0900

    Update 1181 단언 정렬.cpp

--- 백준/1181 단언 정렬.cpp
+++ 백준/1181 단언 정렬.cpp
@@ -28,3 +28,33 @@ int main(){
         cout<<v[i]<<"\n";
     }
 }
+#include <iostream>
+#include<algorithm>
+#include<string>
+#include <vector>
+using namespace std;
+bool Compare(string a, string b){
+    int A=a.size();
+    int B=b.size();
+    if(A!=B)return A<B;
+    
+    return a<b;
+}
+int main(){
+    int t;
+    cin>>t;
+    vector<string> v;
+    for(int i=0; i<t; i++){
+        string s;
+        cin>>s;
+        if(find(v.begin(),v.end(), s) == v.end())
+          v.push_back(s);
+
+    }
+    sort(v.begin(),v.end(),Compare);
+    for(int i=0; i<v.size(); i++){
+        
+        cout<<v[i]<<"\n";
+    }
+}
+
