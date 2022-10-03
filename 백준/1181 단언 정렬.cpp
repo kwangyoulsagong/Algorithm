f1d44a1e64b210004da5dd96d5eb1e8a1b33489d
Tkrhdrhkdduf <61825101+Tkrhdrhkdduf@users.noreply.github.com>
2022-10-03 17:56:02 +0900

    Create 1181 단언 정렬.cpp

---
+++ 백준/1181 단언 정렬.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<algorithm>
+#include<string>
+#include <vector>
+using namespace std;
+bool Compare(string a, string b){
+    int A=a.size();
+    int B=b.size();
+    if(A!=B)return A<B;
+    
+    return a<b;
+}
+int main(){
+    int t;
+    cin>>t;
+    vector<string> v;
+    for(int i=0; i<t; i++){
+        string s;
+        cin>>s;
+        v.push_back(s);
+        
+    }
+    sort(v.begin(),v.end(),Compare);
+    for(int i=0; i<v.size(); i++){
+        if(v[i]==v[i+1]){
+            continue;
+        }
+        cout<<v[i]<<"\n";
+    }
+}
