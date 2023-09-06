4ddf644b153c66006384c2d4551d4af9913bcedd
Kwangyoulsagong <61825101+kwangyoulsagong@users.noreply.github.com>
2023-09-06 14:55:22 +0900

    Add files via upload

---
+++ algorithm/bronzeV/꼬마정민.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long a, b, c;
+    long long sum=0;
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    cout<<sum<<endl;
+    return 0;
+    
+}
