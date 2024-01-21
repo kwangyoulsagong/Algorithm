232aeedf792f786be42498db3dbba8d3342757d2
Kwangyoulsagong <61825101+kwangyoulsagong@users.noreply.github.com>
2024-01-21 23:56:08 +0900

    Add files via upload

---
+++ js-programmers/level0/배열조각하기.js
@@ -0,0 +1,10 @@
+function solution(arr, query) {
+  for (let i = 0; i < query.length; i++) {
+    if (i % 2 == 0) {
+      arr.splice(query[i] + 1, arr.length - (query[i] + 1));
+    } else {
+      arr.splice(0, query[i]);
+    }
+  }
+  return arr;
+}
