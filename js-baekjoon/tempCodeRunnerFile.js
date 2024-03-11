4317239001b8b4314362ef0f82b2554fbe48b281
kwangyoulsagong <sgky0511@naver.com>
2024-03-11 23:57:16 +0900

    feat: 토마토

--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -1,30 +1,5 @@
-const fs = require("fs");
-const file = process.platform === "linux" ? "dev/stdin" : "./example.txt";
-// let [n, ...arr] = fs.readFileSync(file).toString().trim().split("\n");
-let input = fs.readFileSync(file).toString().trim().split("\n");
-const inputNum = [];
-
-let count = 0;
-const result = [];
-for (let i = 0; i < 2; i++) {
-  inputNum.push(input[count++].split(" ").join(" "));
-  const name = [];
-  const value = [];
-
-  for (let j = 0; j < Number(inputNum[i]); j++) {
-    name.push(input[count++].split(" ").join(" "));
-  }
-  for (let k = 0; k < 2 * inputNum[i] - 1; k++) {
-    const [a, b] = input[count++].split(" ");
-    value.push(a);
-  }
-  value.sort();
-  for (let n = 0; n < value.length; n++) {
-    if (value[n] != value[n + 1] && value[n - 1] != value[n]) {
-      result.push(name[value[n] - 1]);
-    }
-  }
-}
-for (let i = 0; i < result.length; i++) {
-  console.log(`${i + 1} ${result[i]}`);
-}
+  // arr.forEach((tomato, index) => {
+    //   if (tomato == 1) {
+    //     queue.push([i - 1, index]);
+    //   }
+    // });
