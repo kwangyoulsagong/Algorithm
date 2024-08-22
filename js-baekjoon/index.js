a1db15e32ab1cfbbb63e3a24ac719b84227e884d
kwangyoulsagong <sgky0511@naver.com>
2024-08-22 16:21:52 +0900

    feat: 10952 A+B-5

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -5,8 +5,10 @@ const input = fs.readFileSync(file).toString().trim().split("\n");
 const solution = (a, b) => {
   return a + b;
 };
-
-for (let i = 0; i < input.length; i++) {
+let i = 0;
+for (;;) {
   const [a, b] = input[i].split(" ").map(Number);
+  if (a == 0 && b == 0) break;
   console.log(solution(a, b));
+  i++;
 }
