28472efbf836c713c5787c6f86337e07aee9008a
kwangyoulsagong <sgky0511@naver.com>
2024-08-22 13:57:33 +0900

    feat: 2739 구구단

--- js-baekjoon/bronzeV/2739구구단.js
+++ js-baekjoon/bronzeV/2739구구단.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
 
-const solution = (a, b) => {};
-const [a, b] = input[0].split(" ").map(Number);
-solution(a, b);
+const solution = (t) => {
+  for (let i = 1; i <= 9; i++) {
+    console.log(`${t} * ${i} = ${t * i}`);
+  }
+};
+const [t] = input[0].split(" ").map(Number);
+solution(t);
