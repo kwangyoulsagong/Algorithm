cccfba14579d2fc95cbb246636620518010ccb24
kwangyoulsagong <sgky0511@naver.com>
2024-08-22 14:01:16 +0900

    feat: 10869 사칙연산

---
+++ js-baekjoon/bronzeV/10869사칙연산.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const input = fs.readFileSync(file).toString().trim().split("\n");
+
+const solution = (a, b) => {
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(parseInt(a / b));
+  console.log(a % b);
+};
+const [a, b] = input[0].split(" ").map(Number);
+solution(a, b);
--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -2,6 +2,12 @@ const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
 
-const solution = (a, b) => {};
+const solution = (a, b) => {
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(parseInt(a / b));
+  console.log(a % b);
+};
 const [a, b] = input[0].split(" ").map(Number);
 solution(a, b);
