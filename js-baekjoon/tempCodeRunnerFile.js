3a47875669a6a184d88db641067f55d84a688479
kwangyoulsagong <sgky0511@naver.com>
2024-08-28 22:10:56 +0900

    feat: 11720 숫자의합

--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -1,10 +1,1 @@
-const fs = require("fs");
-const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
-const input = fs.readFileSync(file).toString().trim().split("\n");
-
-const solution = (a, b) => {
-  a > b ? console.log(">") : console.log("<");
-  if (a == b) console.log("=");
-};
-const [a, b] = input[0].split(" ").map(Number);
-solution(a, b);
+console.log(result);
