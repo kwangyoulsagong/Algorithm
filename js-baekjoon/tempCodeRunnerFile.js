79540811b50bbb9544a97f0024882ee218ca36c3
kwangyoulsagong <sgky0511@naver.com>
2024-12-30 12:21:06 +0900

    feat: 4158 cd js

--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -1,32 +1,37 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
+const [n, m] = input.shift().split(" ").map(Number);
 
-const [n, m, k, s] = input.shift().split(" ").map(Number);
-const graph = Array.from({ length: n + 1 }, () => []);
-for (let a of input) {
-  const [u, v] = a.split(" ").map(Number);
-  graph[u].push(v);
+const sang = [];
+for (let i = 0; i < n; i++) {
+  const [v] = input.shift();
+  sang.push(parseInt(v));
 }
 
-const distance = Array.from({ length: n + 1 }, () => -1);
-distance[s] = 0;
-const queue = [s];
-while (queue.length) {
-  const current = queue.shift();
-  for (let a of graph[current]) {
-    if (distance[a] === -1) {
-      distance[a] = distance[current] + 1;
-      queue.push(a);
+const sun = [];
+for (let i = 0; i < m; i++) {
+  const [v] = input.shift();
+  sun.push(parseInt(v));
+}
+const binary_search = (sang, sun, target, start, end) => {
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (sang[mid] == target && sun[mid] == target) {
+      return true;
+    } else if (sang[mid] > target && sun[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
     }
   }
-}
+  return false;
+};
 
-const result = [];
-for (let i = 1; i <= n; i++) {
-  if (distance[i] === k) {
-    result.push(i);
+let count = 0;
+for (let i of sang) {
+  if (binary_search(sang, sun, i, 0, n - 1)) {
+    count += 1;
   }
 }
-
-console.log(result.length ? result.join("\n") : -1);
+console.log(count);
