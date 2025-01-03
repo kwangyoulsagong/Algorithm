3f5cf9bd242e9d5dbccade7445aac1aa96c41df3
kwangyoulsagong <sgky0511@naver.com>
2025-01-03 15:37:01 +0900

    feat 상 하 좌 우

--- 이것이코딩테스트/구현/ex1상하좌우.js
+++ 이것이코딩테스트/구현/ex1상하좌우.js
@@ -7,6 +7,68 @@
 //L,R,U,D
 //여행자가 NXN 크기의 정사각형 공간을 벗어나는 움직음은 무시
 //예시 n=5 R R R U D D 1,1-> 1,2 1,3 1,4 2,4 3,4로 도착
-
+// 내풀이
 const n = 5;
 const route = ["R", "R", "R", "U", "D", "D"];
+
+// const direction = {
+//   L: [0, -1],
+//   R: [0, 1],
+//   U: [-1, 0],
+//   D: [1, 0],
+// };
+// let point = [[0, 0]];
+// const bfs = () => {
+//   for (let i = 0; i < route.length; i++) {
+//     let [startx, starty] = point.shift();
+
+//     const [x, y] = direction[route[i]];
+//     const newx = startx + x;
+//     const newy = starty + y;
+//     if (newx >= 0 && newy >= 0 && newx < n && newy < n) {
+//       point.push([newx, newy]);
+//     } else {
+//       point.push([startx, starty]);
+//     }
+//   }
+// };
+// bfs();
+// const [x, y] = point.shift();
+// console.log(x + 1, y + 1);
+
+// 책 풀이
+// const dx = [0, 0, -1, 1];
+// const dy = [-1, 1, 0, 0];
+// const directon = ["L", "R", "U", "D"];
+// let x = 0;
+// let y = 0;
+// for (const moves of route) {
+//   for (let i = 0; i < 4; i++) {
+//     if (moves == directon[i]) {
+//       const nx = x + dx[i];
+//       const ny = y + dy[i];
+//       if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+//         x = nx;
+//         y = ny;
+//       }
+//     }
+//   }
+// }
+// console.log(x + 1, y + 1);
+
+// 고도화 버전
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
+const directon = ["L", "R", "U", "D"];
+let x = 0;
+let y = 0;
+for (const moves of route) {
+  const i = directon.indexOf(moves);
+  const nx = x + dx[i];
+  const ny = y + dy[i];
+  if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+    x = nx;
+    y = ny;
+  }
+}
+console.log(x + 1, y + 1);
