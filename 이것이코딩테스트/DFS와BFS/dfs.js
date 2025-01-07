be1320ae5f8bab9773b75742538845f7988544a3
kwangyoulsagong <sgky0511@naver.com>
2025-01-07 16:43:05 +0900

    feat: DFS

---
+++ 이것이코딩테스트/DFS와BFS/dfs.js
@@ -0,0 +1,24 @@
+const dfs = (graph, v, visited) => {
+  visited[v] = true;
+  process.stdout.write(v + " ");
+  for (let i = 0; i < graph[v].length; i++) {
+    const neighbor = graph[v][i];
+    if (!visited[neighbor]) {
+      dfs(graph, neighbor, visited);
+    }
+  }
+};
+
+const graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7],
+];
+const visited = Array.from({ length: graph.length + 1 }, () => false);
+dfs(graph, 1, visited);
