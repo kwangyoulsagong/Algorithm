7c074187566e6b6b62a2a3f74176e3969451ea32
kwangyoulsagong <sgky0511@naver.com>
2025-01-22 16:16:09 +0900

    암기왕

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,14 +1,29 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const n = parseInt(input);
-const dp = Array(n + 1).fill(BigInt(0));
-
-dp[0] = BigInt(0);
-dp[1] = BigInt(1);
-
-for (let i = 2; i <= n; i++) {
-  dp[i] = dp[i - 1] + dp[i - 2];
+const T = Number(input.shift());
+let idx = 0;
+const binarySearch = (arr, value) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (value == arr[mid]) {
+      return 1;
+    } else if (value > arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return 0;
+};
+for (let t = 0; t < T; t++) {
+  const v1 = Number(input[idx++]);
+  const note1 = input[idx++].split(" ").map(Number);
+  const v2 = Number(input[idx++]); // 수첩2 크기
+  const note2 = input[idx++].split(" ").map(Number);
+  note1.sort((a, b) => a - b);
+  const result = note2.map((v) => binarySearch(note1, v));
+  console.log(result.join("\n"));
 }
-
-console.log(dp[n].toString());
