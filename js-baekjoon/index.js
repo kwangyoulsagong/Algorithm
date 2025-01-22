b4b3a811f2a5faa78af3d14dafc638b1bb7f0235
kwangyoulsagong <sgky0511@naver.com>
2025-01-22 14:44:31 +0900

    feat: 10826 피보나치수4

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,22 +1,14 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n, k] = input.shift().split(" ").map(Number);
-const z = Math.floor((100 * k) / n);
-let left = 0;
-let right = n;
-let res = 0;
-if (z >= 99) {
-  console.log(-1);
-} else {
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (Math.floor((100 * (k + mid)) / (n + mid)) > z) {
-      res = mid;
-      right = mid - 1;
-    } else {
-      left = mid + 1;
-    }
-  }
-  console.log(res);
+const n = parseInt(input);
+const dp = Array(n + 1).fill(BigInt(0));
+
+dp[0] = BigInt(0);
+dp[1] = BigInt(1);
+
+for (let i = 2; i <= n; i++) {
+  dp[i] = dp[i - 1] + dp[i - 2];
 }
+
+console.log(dp[n].toString());
