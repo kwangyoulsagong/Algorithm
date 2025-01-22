ee47b23089c4412f8a2aa97fd395bcef42af7517
kwangyoulsagong <sgky0511@naver.com>
2025-01-22 14:16:40 +0900

    feat: 1072 게임

--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -1,5 +1,21 @@
-for (let i = 0; i < k; k++) {
-//   for (let j = 0; j < n; j++) {
-//     concent[0].push(i);
-//   }
-// }
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const input = fs.readFileSync(file).toString().trim().split("\n");
+const [n, k] = input.shift().split(" ").map(Number);
+const z = Math.floor((100 * k) / n);
+let left = 0;
+let right = n;
+let res = 0;
+if (z >= 99) {
+  console.log(-1);
+}
+while (left <= right) {
+  const mid = Math.floor((left + right) / 2);
+  if (Math.floor((100 * (k + mid)) / n + mid) > z) {
+    res = mid;
+    right = mid - 1;
+  } else {
+    left = mid + 1;
+  }
+}
+console.log(res);
