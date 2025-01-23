10c5f6c080ee2aea4da3d87cce81ab16c1f937e7
kwangyoulsagong <sgky0511@naver.com>
2025-01-23 17:46:26 +0900

    feat: 최소 회의실 개수 복습

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,50 +1,19 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n, m, t] = input.shift().split(" ").map(Number);
-const map = input.map((v) => v.split(" ").map(Number));
-const visited = Array.from({ length: n }, () =>
-  Array.from({ length: m }, () => Array(2).fill(false))
-);
-visited[0][0][0] = true;
-
-const distance = Array.from({ length: n }, () =>
-  Array.from({ length: m }, () => Array(2).fill(Infinity))
-);
-distance[0][0][0] = 0;
-const queue = [[0, 0, 0]];
-const dx = [0, 0, -1, 1];
-const dy = [1, -1, 0, 0];
-
-const bfs = () => {
-  while (queue.length > 0) {
-    const [x, y, item] = queue.shift();
-    for (let i = 0; i < 4; i++) {
-      const newx = x + dx[i];
-      const newy = y + dy[i];
-      if (newx >= 0 && newy >= 0 && newx < n && newy < m) {
-        const value = map[newx][newy];
-        const newItem = item || value == 2 ? 1 : 0;
-        if (
-          !visited[newx][newy][newItem] &&
-          (value == 0 || value == 2 || (value == 1 && item == 1))
-        ) {
-          visited[newx][newy][newItem] = true;
-          distance[newx][newy][newItem] = distance[x][y][item] + 1;
-          queue.push([newx, newy, newItem]);
-        }
-      }
-    }
-  }
-};
-
-bfs();
-const resultWithoutWeapon = distance[n - 1][m - 1][0];
-const resultWithWeapon = distance[n - 1][m - 1][1];
-const finalResult = Math.min(resultWithoutWeapon, resultWithWeapon);
-
-if (finalResult <= t) {
-  console.log(finalResult);
-} else {
-  console.log("Fail");
+const [n] = input.shift().split(" ").map(Number);
+const time = input.map((v) => v.split(" ").map(Number));
+const timeArr = [];
+for (let i = 0; i < n; i++) {
+  const [s, e] = time[i];
+  timeArr.push([s, 1]);
+  timeArr.push([e, -1]);
 }
+timeArr.sort((a, b) => (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
+let result = 0;
+let count = 0;
+timeArr.forEach((v) => {
+  count += v[1];
+  result = Math.max(result, count);
+});
+console.log(result);
