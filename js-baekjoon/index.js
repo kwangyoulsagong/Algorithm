508f33b367d32ca125d29da66bcb7f77c1b85156
kwangyoulsagong <sgky0511@naver.com>
2025-01-23 15:31:10 +0900

    feat: dfs 복습

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,29 +1,37 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const T = Number(input.shift());
-let idx = 0;
-const binarySearch = (arr, value) => {
-  let start = 0;
-  let end = arr.length - 1;
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    if (value == arr[mid]) {
-      return 1;
-    } else if (value > arr[mid]) {
-      start = mid + 1;
-    } else {
-      end = mid - 1;
+const [n, m] = input.shift().split(" ").map(Number);
+const map = input.map((v) => v.split("").map(Number));
+
+let count = 0;
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
+const visited = Array.from({ length: n + 1 }, () => Array(m + 1).fill(false));
+
+const dfs = (x, y) => {
+  visited[x][y] = true;
+  for (let i = 0; i < 4; i++) {
+    const newx = x + dx[i];
+    const newy = y + dy[i];
+    if (
+      newx >= 0 &&
+      newy >= 0 &&
+      newx < n &&
+      newy < m &&
+      map[newx][newy] != 1 &&
+      !visited[newx][newy]
+    ) {
+      dfs(newx, newy);
     }
   }
-  return 0;
 };
-for (let t = 0; t < T; t++) {
-  const v1 = Number(input[idx++]);
-  const note1 = input[idx++].split(" ").map(Number);
-  const v2 = Number(input[idx++]); // 수첩2 크기
-  const note2 = input[idx++].split(" ").map(Number);
-  note1.sort((a, b) => a - b);
-  const result = note2.map((v) => binarySearch(note1, v));
-  console.log(result.join("\n"));
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    if (map[i][j] != 1 && !visited[i][j]) {
+      dfs(i, j);
+      count += 1;
+    }
+  }
 }
+console.log(count);
