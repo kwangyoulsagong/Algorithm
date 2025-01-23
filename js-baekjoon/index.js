d27e32ac5d2fe552b6085620c6ab1e296d6c2882
kwangyoulsagong <sgky0511@naver.com>
2025-01-23 15:57:58 +0900

    feat: dfs  복습

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -4,34 +4,29 @@ const input = fs.readFileSync(file).toString().trim().split("\n");
 const [n, m] = input.shift().split(" ").map(Number);
 const map = input.map((v) => v.split("").map(Number));
 
-let count = 0;
 const dx = [0, 0, -1, 1];
 const dy = [-1, 1, 0, 0];
-const visited = Array.from({ length: n + 1 }, () => Array(m + 1).fill(false));
-
-const dfs = (x, y) => {
-  visited[x][y] = true;
-  for (let i = 0; i < 4; i++) {
-    const newx = x + dx[i];
-    const newy = y + dy[i];
-    if (
-      newx >= 0 &&
-      newy >= 0 &&
-      newx < n &&
-      newy < m &&
-      map[newx][newy] != 1 &&
-      !visited[newx][newy]
-    ) {
-      dfs(newx, newy);
+const queue = [[0, 0]];
+const bfs = () => {
+  while (queue.length) {
+    const [x, y] = queue.shift();
+    console.log(x, y);
+    for (let i = 0; i < 4; i++) {
+      const newx = x + dx[i];
+      const newy = y + dy[i];
+      if (
+        newx >= 0 &&
+        newy >= 0 &&
+        newx < n &&
+        newy < m &&
+        map[newx][newy] == 1
+      ) {
+        map[newx][newy] = map[newx][newy] + map[x][y];
+        queue.push([newx, newy]);
+      }
     }
   }
 };
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < m; j++) {
-    if (map[i][j] != 1 && !visited[i][j]) {
-      dfs(i, j);
-      count += 1;
-    }
-  }
-}
-console.log(count);
+
+bfs();
+console.log(map[n - 1][m - 1]);
