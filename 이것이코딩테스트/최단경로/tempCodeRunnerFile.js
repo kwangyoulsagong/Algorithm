e0f8b7c260014fbe285d63b9aec030973590d6c6
kwangyoulsagong <sgky0511@naver.com>
2025-02-03 14:27:41 +0900

    feat: 우선순위큐 다익스트라

--- 이것이코딩테스트/최단경로/tempCodeRunnerFile.js
+++ 이것이코딩테스트/최단경로/tempCodeRunnerFile.js
@@ -1,12 +1,7 @@
-// 방문하지 않는 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
-const get_smallest_node = () => {
-  let minValue = Infinity;
-  let index = 0;
-  for (let i in distance) {
-    if (!visited[i] && distance[i] < minValue) {
-      minValue = distance[i];
-      index = i;
-    }
-  }
-  return index;
-};
+// 노드의 개수 간선의 개수
+const [n, m] = nm.split(" ").map(Number);
+
+// 시작 노드 번호
+const start = parseInt(s);
+
+const arr = rest.map((str) => str.split(" ").map(Number));
---
+++ 이것이코딩테스트/최단경로/우선순위큐다익스트라.js
@@ -0,0 +1,53 @@
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push([val, priority]);
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a[1] - b[1]);
+  }
+}
+
+const fs = require("fs");
+let input = fs.readFileSync("./example.txt").toString().trim().split("\n");
+let [nm, s, ...rest] = input;
+// 노드의 개수 간선의 개수
+const [n, m] = nm.split(" ").map(Number);
+
+// 시작 노드 번호
+const start = parseInt(s);
+
+const arr = rest.map((str) => str.split(" ").map(Number));
+const graph = Array.from({ length: n + 1 }, () => []);
+const distance = Array.from({ length: n + 1 }, () => Infinity);
+for (const value of arr) {
+  const [a, b, c] = value;
+  graph[a].push([b, c]);
+}
+
+const dijkstra = (start) => {
+  const pq = new PriorityQueue();
+  pq.enqueue(start, 0);
+  distance[start] = 0;
+  while (pq.values.length > 0) {
+    const [now, dist] = pq.dequeue();
+    if (distance[now] < dist) continue;
+    for (let [next, cost] of graph[now]) {
+      const newCost = dist + cost;
+      if (newCost < distance[next]) {
+        distance[next] = newCost;
+        pq.enqueue(next, newCost);
+      }
+    }
+  }
+};
+dijkstra(start);
+for (let i = 1; i <= n; i++) {
+  console.log(distance[i] === Infinity ? "INFINITY" : distance[i]);
+}
