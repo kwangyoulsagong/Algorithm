82ff0f6dcd1eecc4549a8cc4692f8b6139eca5be
kwangyoulsagong <sgky0511@naver.com>
2025-02-04 13:42:48 +0900

    feat: 전보

--- 이것이코딩테스트/최단경로/tempCodeRunnerFile.js
+++ 이것이코딩테스트/최단경로/tempCodeRunnerFile.js
@@ -13,23 +13,17 @@ class PriorityQueue {
     this.values.sort((a, b) => a[1] - b[1]);
   }
 }
-
 const fs = require("fs");
 let input = fs.readFileSync("./example.txt").toString().trim().split("\n");
-let [nm, s, ...rest] = input;
-// 노드의 개수 간선의 개수
-const [n, m] = nm.split(" ").map(Number);
-
-// 시작 노드 번호
-const start = parseInt(s);
-
-const arr = rest.map((str) => str.split(" ").map(Number));
+let [nmc, ...rest] = input;
+const [n, m, c] = nmc.split(" ").map(Number);
+const arr = rest.map((v) => v.split(" ").map(Number));
 const graph = Array.from({ length: n + 1 }, () => []);
 const distance = Array.from({ length: n + 1 }, () => Infinity);
-for (const value of arr) {
-  const [a, b, c] = value;
+for (const val of arr) {
+  const [a, b, c] = val;
   graph[a].push([b, c]);
-} 
+}
 const dijkstra = (start) => {
   const pq = new PriorityQueue();
   pq.enqueue(start, 0);
@@ -37,7 +31,7 @@ const dijkstra = (start) => {
   while (pq.values.length > 0) {
     const [now, dist] = pq.dequeue();
     if (distance[now] < dist) continue;
-    for (let [next, cost] of graph[now]) {
+    for (const [next, cost] of graph[now]) {
       const newCost = dist + cost;
       if (newCost < distance[next]) {
         distance[next] = newCost;
@@ -46,7 +40,13 @@ const dijkstra = (start) => {
     }
   }
 };
-dijkstra(start);
-for (let i = 1; i <= n; i++) {
-  console.log(distance[i] === Infinity ? "INFINITY" : distance[i]);
+dijkstra(c);
+let count = 0;
+let result = 0;
+for (let val of distance) {
+  if (val != Infinity) {
+    count++;
+    result = Math.max(result, val);
+  }
 }
+console.log(count - 1, result);
