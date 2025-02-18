3238ba43a3f20dff9c1212ddb81a150a7b8750ca
kwangyoulsagong <sgky0511@naver.com>
2025-02-18 14:15:35 +0900

    feat: 문자열 압축 복습

--- js-programmers/level2/문자열압축.js
+++ js-programmers/level2/문자열압축.js
@@ -1,5 +1,5 @@
 function solution(s) {
-  if (s.length === 1) return 1;
+  if (s.length == 1) return 1;
   var answer = Infinity;
   const length = Math.floor(s.length / 2);
   for (let i = 1; i <= length; i++) {
@@ -7,14 +7,16 @@ function solution(s) {
     let count = 1;
     let prev = s.slice(0, i);
     for (let j = i; j < s.length; j += i) {
-      if (prev === s.slice(j, j + i)) count++;
-      else {
+      if (prev === s.slice(j, j + i)) {
+        count++;
+      } else {
         count >= 2 ? (sentence += count + prev) : (sentence += prev);
         prev = s.slice(j, j + i);
         count = 1;
       }
     }
     count >= 2 ? (sentence += count + prev) : (sentence += prev);
+
     answer = Math.min(answer, sentence.length);
   }
   return answer;
