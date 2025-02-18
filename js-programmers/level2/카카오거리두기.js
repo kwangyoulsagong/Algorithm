208bf9dc534a68b8077f18a631f9f69fa58d6249
kwangyoulsagong <sgky0511@naver.com>
2025-02-18 14:14:59 +0900

    feat: 카카오 거리두기 복습

--- js-programmers/level2/카카오거리두기.js
+++ js-programmers/level2/카카오거리두기.js
@@ -1,11 +1,9 @@
-const bfs = (arr, startx, starty) => {
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, 1, 0, -1];
-  const map = arr.map((v) => v.split(""));
+const dx = [-1, 0, 1, 0];
+const dy = [0, 1, 0, -1];
+
+const bfs = (startx, starty, map) => {
   const queue = [[startx, starty]];
-  const visited = Array.from({ length: 5 }, () =>
-    Array.from({ length: 5 }, () => false)
-  );
+  const visited = Array.from({ length: 5 }, () => Array(5).fill(false));
   visited[startx][starty] = true;
   while (queue.length > 0) {
     const [x, y] = queue.shift();
@@ -20,30 +18,29 @@ const bfs = (arr, startx, starty) => {
         !visited[newx][newy]
       ) {
         const value = Math.abs(startx - newx) + Math.abs(starty - newy);
-        if (map[newx][newy] == "X") {
-          continue;
-        } else if (map[newx][newy] == "P" && value <= 2) {
-          return 0;
-        } else {
+        if (map[newx][newy] === "X") continue;
+        else if (map[newx][newy] === "P" && value <= 2) return 0;
+        else {
           visited[newx][newy] = true;
           if (value < 2) queue.push([newx, newy]);
         }
       }
     }
   }
-
   return 1;
 };
 
 function solution(places) {
-  const answer = [];
+  var answer = [];
+
   for (let i = 0; i < places.length; i++) {
+    const map = places[i].map((v) => v.split(""));
     let safe = 1;
-    for (let x = 0; x < 5; x++) {
-      for (let y = 0; y < 5; y++) {
-        if (places[i][x][y] == "P") {
-          const value = bfs(places[i], x, y);
-          if (value == 0) {
+    for (let j = 0; j < 5; j++) {
+      for (let k = 0; k < 5; k++) {
+        if (map[j][k] === "P") {
+          const value = bfs(j, k, map);
+          if (value === 0) {
             safe = 0;
             break;
           }
