bc51ad80c75a8ae53e2d4d8401b974d4ea5ce1c5
kwangyoulsagong <sgky0511@naver.com>
2025-02-20 12:59:19 +0900

    feat: 탈출

--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -3,52 +3,69 @@ const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
 const [n] = input.shift().split(" ").map(Number);
 
-const map = input.map((v) => v.split(" ").map(String));
-const dx = [-1, 0, 1, 0];
-const dy = [0, -1, 0, 1];
-let flag = false;
-const checkSight = (arr) => {
-  const queue = [];
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (arr[i][j] === "T") {
-        queue.push([i, j]);
-      }
-    }
-  }
-
-  for (let i = 0; i < queue.length; i++) {
-    for (let j = 0; j < 4; j++) {
-      let [x, y] = queue[i];
-      while (x >= 0 && y >= 0 && x < n && y < n) {
-        if (arr[x][y] == "O") break;
-        else if (arr[x][y] === "S") return false;
-        x += dx[j];
-        y += dy[j];
-      }
-    }
-  }
-  return true;
-};
-const dfs = (cnt) => {
-  if (cnt == 3) {
-    let result = checkSight(map.map((v) => [...v]));
-    if (result) {
-      flag = true;
-      return;
-    }
-  } else {
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n; j++) {
-        if (map[i][j] === "X") {
-          map[i][j] = "O";
-          dfs(cnt + 1);
-          map[i][j] = "X";
+const map = input.map((v) => v.split("").map(String));
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+const water = [];
+const bfs = (startx, starty, map, water) => {
+  const visited = Array.from({ length: n }, () => Array(n).fill(0));
+  console.log(water);
+  visited[startx][starty] = 0;
+  const queue = [[startx, starty]];
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
+    const [wx, wy] = water.shift();
+    for (let i = 0; i < 4; i++) {
+      const newx = x + dx[i];
+      const newy = y + dy[i];
+      if (
+        newx >= 0 &&
+        newy >= 0 &&
+        newx < n &&
+        newy < n &&
+        visited[newx][newy] == 0
+      ) {
+        if (map[newx][newy] === "D") {
+          visited[newx][newy] = visited[x][y] + 1;
+          return visited[newx][newy];
+        }
+        if (map[newx][newy] !== "*" && map[newx][newy] !== "X") {
+          visited[newx][newy] = visited[x][y] + 1;
+          queue.push([newx, newy]);
         }
       }
     }
+    for (let i = 0; i < 4; i++) {
+      const newx = wx + dx[i];
+      const newy = wy + dy[i];
+      if (
+        newx >= 0 &&
+        newy >= 0 &&
+        newx < n &&
+        newy < n &&
+        map[newx][newy] == "."
+      ) {
+        map[newx][newy] = "*";
+        water.push([newx, newy]);
+      }
+    }
   }
+  return "KAKTUS";
 };
-dfs(0);
-if (flag) console.log("YES");
-else console.log("NO");
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    if (map[i][j] == "*") {
+      water.push([i, j]);
+    }
+  }
+}
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    if (map[i][j] === "S") {
+      const result = bfs(i, j, map, water);
+      console.log(result);
+    }
+  }
+}
