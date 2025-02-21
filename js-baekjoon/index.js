8a985bafa5c9d455e7a8959389855f4413bc9958
kwangyoulsagong <sgky0511@naver.com>
2025-02-21 16:31:12 +0900

    feat: 동전 2

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,26 +1,19 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n] = input.shift().split(" ").map(Number);
-const arr = input[0].split(" ").map(Number);
-arr.sort((a, b) => a - b);
-let closest = Infinity;
-let result = [0, 0];
-
-let start = 0;
-let end = n - 1;
-while (start < end) {
-  let sum = arr[start] + arr[end];
-  if (Math.abs(sum) < closest) {
-    closest = Math.abs(sum);
-    result[0] = arr[start];
-    result[1] = arr[end];
-  }
-  if (sum < 0) {
-    start++;
-  } else {
-    end--;
+const [n, m] = input.shift().split(" ").map(Number);
+const arr = input.map(Number);
+const dp = Array(m + 1).fill(10001);
+dp[0] = 0;
+for (let i = 0; i < n; i++) {
+  for (let j = arr[i]; j <= m; j++) {
+    if (dp[j - arr[i]] !== 10001) {
+      dp[j] = Math.min(dp[j], dp[j - arr[i]] + 1);
+    }
   }
 }
-
-console.log(result[0], result[1]);
+if (dp[m] === 10001) {
+  console.log(-1);
+} else {
+  console.log(dp[m]);
+}
