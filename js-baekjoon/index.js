b8c1e870a57c4bc3de50ae3d8aa6d0232e01efc6
kwangyoulsagong <sgky0511@naver.com>
2025-03-08 17:15:05 +0900

    feat: 좋다

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,11 +1,41 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n] = input.shift().split(" ").map(Number);
-const dp = Array(n + 1).fill(0);
-dp[1] = 1;
-dp[2] = 3;
-for (let i = 3; i < n + 1; i++) {
-  dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
-}
-console.log(dp[n]);
+const [n] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+arr.sort((a, b) => a - b);
+const findGoodNum = (num, i) => {
+  let start = 0;
+  let end = n - 1;
+  let count = 0;
+  while (start < end) {
+    let sum = 0;
+    if (start === i) {
+      start++;
+      continue;
+    }
+
+    if (end === i) {
+      end--;
+      continue;
+    }
+
+    sum = arr[start] + arr[end];
+    if (sum === num) {
+      count++;
+      break;
+    } else if (sum < num) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+  return count;
+};
+
+let count = 0;
+arr.forEach((cur, i) => {
+  const result = findGoodNum(cur, i);
+  if (result) count++;
+});
+console.log(count);
