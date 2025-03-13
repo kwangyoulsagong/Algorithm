592799e29614a65e3f17fd8b541280f4661edba2
kwangyoulsagong <sgky0511@naver.com>
2025-03-13 15:43:03 +0900

    feat: 인구 이동

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,15 +1,65 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n] = input.shift().split(" ").map(Number);
-const arr = input.map((v) => v.split(" ").map(Number));
-const dp = Array(n).fill(0);
-for (let i = 0; i < n; i++) {
-  const [day, cost] = arr[i];
-  if (i + day > n) continue;
-  dp[i] += cost;
-  for (let j = i + day; j < n; j++) {
-    dp[j] = Math.max(dp[i], dp[j]);
+const [n, l, r] = input.shift().split(" ").map(Number);
+const map = input.map((v) => v.split(" ").map(Number));
+
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+const bfs = (startx, starty, visited, flag) => {
+  const queue = [[startx, starty]];
+  visited[startx][starty] = true;
+  let cnt = 1;
+  let sumPopulation = map[startx][starty];
+  const arr = [[startx, starty]];
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      const newx = x + dx[i];
+      const newy = y + dy[i];
+      if (
+        newx >= 0 &&
+        newy >= 0 &&
+        newx < n &&
+        newy < n &&
+        !visited[newx][newy]
+      ) {
+        const difference = Math.abs(map[x][y] - map[newx][newy]);
+        if (difference >= l && difference <= r) {
+          sumPopulation += map[newx][newy];
+          cnt++;
+          queue.push([newx, newy]);
+          arr.push([newx, newy]);
+          flag.value = true;
+          visited[newx][newy] = true;
+        }
+      }
+    }
   }
+  return { arr, sumPopulation, cnt };
+};
+
+const changePopulation = (arr, sum, cnt) => {
+  let average = Math.floor(sum / cnt);
+  for (const [x, y] of arr) {
+    map[x][y] = average;
+  }
+};
+let result = 0;
+while (true) {
+  const visited = Array.from({ length: n }, () => Array(n).fill(false));
+  let flag = { value: false };
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (!visited[i][j]) {
+        const { arr, sumPopulation, cnt } = bfs(i, j, visited, flag);
+        if (arr.length > 0) {
+          changePopulation(arr, sumPopulation, cnt);
+        }
+      }
+    }
+  }
+  if (!flag.value) break;
+  result++;
 }
-console.log(Math.max(...dp));
+console.log(result);
