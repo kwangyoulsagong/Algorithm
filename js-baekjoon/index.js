aa67556ab39ab814c59bda2029703188b31d47e3
kwangyoulsagong <sgky0511@naver.com>
2025-03-13 13:09:34 +0900

    feat: 퇴사

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,22 +1,15 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n, k] = input.shift().split(" ").map(Number);
+const [n] = input.shift().split(" ").map(Number);
 const arr = input.map((v) => v.split(" ").map(Number));
-const dp = Array.from({ length: n + 1 }, () => Array(k + 1).fill(0));
-
-const findMaxWeight = (w, v, i) => {
-  for (let j = 1; j < k + 1; j++) {
-    if (j - w >= 0) {
-      dp[i][j] = Math.max(dp[i - 1][j - w] + v, dp[i - 1][j]);
-    } else {
-      dp[i][j] = dp[i - 1][j];
-    }
+const dp = Array(n).fill(0);
+for (let i = 0; i < n; i++) {
+  const [day, cost] = arr[i];
+  if (i + day > n) continue;
+  dp[i] += cost;
+  for (let j = i + day; j < n; j++) {
+    dp[j] = Math.max(dp[i], dp[j]);
   }
-};
-
-for (let i = 1; i < n + 1; i++) {
-  const [w, v] = arr[i - 1];
-  findMaxWeight(w, v, i);
 }
-console.log(dp[n][k]);
+console.log(Math.max(...dp));
