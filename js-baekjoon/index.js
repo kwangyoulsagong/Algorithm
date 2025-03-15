a874f35067c8c7d853a782af72d73ee3851ac8ec
kwangyoulsagong <sgky0511@naver.com>
2025-03-15 20:27:20 +0900

    feat: puyo puyo

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,16 +1,13 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n, l, r] = input.shift().split(" ").map(Number);
-const map = input.map((v) => v.split(" ").map(Number));
-
+const map = input.map((v) => v.split("").map(String));
+let result = 0;
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
-const bfs = (startx, starty, visited, flag) => {
+const bfs = (startx, starty, target, visited) => {
   const queue = [[startx, starty]];
   visited[startx][starty] = true;
-  let cnt = 1;
-  let sumPopulation = map[startx][starty];
   const arr = [[startx, starty]];
   while (queue.length > 0) {
     const [x, y] = queue.shift();
@@ -20,46 +17,60 @@ const bfs = (startx, starty, visited, flag) => {
       if (
         newx >= 0 &&
         newy >= 0 &&
-        newx < n &&
-        newy < n &&
-        !visited[newx][newy]
+        newx < 12 &&
+        newy < 6 &&
+        !visited[newx][newy] &&
+        map[newx][newy] === target
       ) {
-        const difference = Math.abs(map[x][y] - map[newx][newy]);
-        if (difference >= l && difference <= r) {
-          sumPopulation += map[newx][newy];
-          cnt++;
-          queue.push([newx, newy]);
-          arr.push([newx, newy]);
-          flag.value = true;
-          visited[newx][newy] = true;
-        }
+        queue.push([newx, newy]);
+        arr.push([newx, newy]);
+        visited[newx][newy] = true;
       }
     }
   }
-  return { arr, sumPopulation, cnt };
+  return arr;
 };
 
-const changePopulation = (arr, sum, cnt) => {
-  let average = Math.floor(sum / cnt);
+const explosion = (arr) => {
   for (const [x, y] of arr) {
-    map[x][y] = average;
+    map[x][y] = ".";
   }
 };
-let result = 0;
+
+const drop = () => {
+  const queue = [];
+
+  for (let j = 0; j < 6; j++) {
+    for (let i = 11; i >= 0; i--) {
+      if (map[i][j] !== ".") {
+        queue.push(map[i][j]);
+        map[i][j] = ".";
+      }
+    }
+    for (let i = 11; i >= 0; i--) {
+      if (queue.length > 0) {
+        const target = queue.shift();
+        map[i][j] = target;
+      }
+    }
+  }
+};
 while (true) {
-  const visited = Array.from({ length: n }, () => Array(n).fill(false));
-  let flag = { value: false };
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (!visited[i][j]) {
-        const { arr, sumPopulation, cnt } = bfs(i, j, visited, flag);
-        if (arr.length > 0) {
-          changePopulation(arr, sumPopulation, cnt);
+  let expload = false;
+  const visited = Array.from({ length: 12 }, () => Array(6).fill(false));
+  for (let i = 0; i < 12; i++) {
+    for (let j = 0; j < 6; j++) {
+      if (map[i][j] != "." && !visited[i][j]) {
+        const arr = bfs(i, j, map[i][j], visited);
+        if (arr.length >= 4) {
+          explosion(arr);
+          expload = true;
         }
       }
     }
   }
-  if (!flag.value) break;
+  if (!expload) break;
+  drop();
   result++;
 }
 console.log(result);
--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -1,35 +1,76 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n] = input[0].split(" ").map(Number);
-const arr = input[1].split(" ").map(Number);
-arr.sort((a, b) => a - b);
-const findGoodNum = (num, i) => {
-  let start = i + 1;
-  let end = n - 1;
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    if (arr[mid] === num) {
-      return true;
-    } else if (arr[mid] < num) {
-      start = mid + 1;
-    } else {
-      end = mid - 1;
+const map = input.map((v) => v.split("").map(String));
+let result = 0;
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+const bfs = (startx, starty, target, visited) => {
+  const queue = [[startx, starty]];
+  visited[startx][starty] = true;
+  const arr = [[startx, starty]];
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      const newx = x + dx[i];
+      const newy = y + dy[i];
+      if (
+        newx >= 0 &&
+        newy >= 0 &&
+        newx < 12 &&
+        newy < 6 &&
+        !visited[newx][newy] &&
+        map[newx][newy] === target
+      ) {
+        queue.push([newx, newy]);
+        arr.push([newx, newy]);
+        visited[newx][newy] = true;
+      }
     }
   }
-  return false;
+  return arr;
 };
 
-let count = 0;
-for (let i = 0; i < n; i++) {
-  const target = arr[i];
-  for (let j = 0; j < i; j++) {
-    const num = target - arr[j];
-    const result = findGoodNum(num, i);
-    if (result) {
-      count++;
-      break;
+const explosion = (arr) => {
+  for (const [x, y] of arr) {
+    map[x][y] = ".";
+  }
+};
+
+const drop = () => {
+  const queue = [];
+
+  for (let j = 0; j < 6; j++) {
+    for (let i = 11; i >= 0; i--) {
+      if (map[i][j] !== ".") {
+        queue.push(map[i][j]);
+        map[i][j] = ".";
+      }
+    }
+    for (let i = 11; i >= 0; i--) {
+      if (queue.length > 0) {
+        const target = queue.shift();
+        map[i][j] = target;
+      }
     }
   }
+};
+while (true) {
+  let expload = false;
+  const visited = Array.from({ length: 12 }, () => Array(6).fill(false));
+  for (let i = 0; i < 12; i++) {
+    for (let j = 0; j < 6; j++) {
+      if (map[i][j] != "." && !visited[i][j]) {
+        const arr = bfs(i, j, map[i][j], visited);
+        if (arr.length >= 4) {
+          explosion(arr);
+          expload = true;
+        }
+      }
+    }
+  }
+  if (!expload) break;
+  drop();
+  result++;
 }
-console.log(count);
+console.log(result);
