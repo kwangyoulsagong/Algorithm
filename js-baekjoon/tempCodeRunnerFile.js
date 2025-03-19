b3c3f8cb95a4e4ace5e8c1970a2fe6f920ed197e
kwangyoulsagong <sgky0511@naver.com>
2025-03-19 14:18:01 +0900

    feat: 숫자 카드 2 이분탐색

--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -1,76 +1,23 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const map = input.map((v) => v.split("").map(String));
+const [n] = input.shift().split(" ").map(Number);
+const arr = input.map((v) => v.split(" ").map(Number));
+const startTime = arr.map((v) => v[0]).sort((a, b) => a - b);
+const endTime = arr.map((v) => v[1]).sort((a, b) => a - b);
+
+let start = 0;
+let end = 0;
+let count = 0;
 let result = 0;
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-const bfs = (startx, starty, target, visited) => {
-  const queue = [[startx, starty]];
-  visited[startx][starty] = true;
-  const arr = [[startx, starty]];
-  while (queue.length > 0) {
-    const [x, y] = queue.shift();
-    for (let i = 0; i < 4; i++) {
-      const newx = x + dx[i];
-      const newy = y + dy[i];
-      if (
-        newx >= 0 &&
-        newy >= 0 &&
-        newx < 12 &&
-        newy < 6 &&
-        !visited[newx][newy] &&
-        map[newx][newy] === target
-      ) {
-        queue.push([newx, newy]);
-        arr.push([newx, newy]);
-        visited[newx][newy] = true;
-      }
-    }
+while (start < n) {
+  if (startTime[start] >= endTime[end] + 10) {
+    end++;
+    count--;
+  } else {
+    start++;
+    count++;
   }
-  return arr;
-};
-
-const explosion = (arr) => {
-  for (const [x, y] of arr) {
-    map[x][y] = ".";
-  }
-};
-
-const drop = () => {
-  const queue = [];
-
-  for (let j = 0; j < 6; j++) {
-    for (let i = 11; i >= 0; i--) {
-      if (map[i][j] !== ".") {
-        queue.push(map[i][j]);
-        map[i][j] = ".";
-      }
-    }
-    for (let i = 11; i >= 0; i--) {
-      if (queue.length > 0) {
-        const target = queue.shift();
-        map[i][j] = target;
-      }
-    }
-  }
-};
-while (true) {
-  let expload = false;
-  const visited = Array.from({ length: 12 }, () => Array(6).fill(false));
-  for (let i = 0; i < 12; i++) {
-    for (let j = 0; j < 6; j++) {
-      if (map[i][j] != "." && !visited[i][j]) {
-        const arr = bfs(i, j, map[i][j], visited);
-        if (arr.length >= 4) {
-          explosion(arr);
-          expload = true;
-        }
-      }
-    }
-  }
-  if (!expload) break;
-  drop();
-  result++;
+  result = Math.max(count, count);
 }
 console.log(result);
