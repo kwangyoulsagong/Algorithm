dbc5ec2263435ac7b820e7cae866da224be9b291
kwangyoulsagong <sgky0511@naver.com>
2025-03-19 15:57:25 +0900

    feat: 뱀

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,39 +1,70 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n] = input[0].split(" ").map(Number);
-const findArr = input[1].split(" ").map(Number);
-const [t] = input[2].split(" ").map(Number);
-const targetArr = input[3].split(" ").map(Number);
-findArr.sort((a, b) => a - b);
-const lowerBound = (num, findArr) => {
-  let start = 0;
-  let end = findArr.length;
-  while (start < end) {
-    const mid = Math.floor((start + end) / 2);
-    if (findArr[mid] >= num) {
-      end = mid;
-    } else {
-      start = mid + 1;
-    }
+const [n] = input.shift(" ").split(" ").map(Number);
+const [apple] = input.shift().split(" ").map(Number);
+const map = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+for (let i = 0; i < apple; i++) {
+  const [x, y] = input.shift().split(" ").map(Number);
+  map[x][y] = 1;
+}
+const [dir] = input.shift().split(" ").map(Number);
+const directions = input.map((v) => v.split(" ").map(String));
+
+const dx = [0, 1, 0, -1];
+const dy = [1, 0, -1, 0];
+
+const checkDir = (route, dir) => {
+  if (route === "L") {
+    return (dir + 3) % 4;
+  } else {
+    return (dir + 1) % 4;
   }
-  return start;
 };
 
-const upperBound = (num, findArr) => {
-  let start = 0;
-  let end = findArr.length;
-  while (start < end) {
-    const mid = Math.floor((start + end) / 2);
-    if (findArr[mid] > num) {
-      end = mid;
+function bfs(map, directions, n, time) {
+  let dir = 0;
+  let x = 1;
+  let y = 1;
+  const queue = [[x, y]];
+  map[x][y] = 2;
+  while (true) {
+    time++;
+    const newx = x + dx[dir];
+    const newy = y + dy[dir];
+    if (
+      newx >= 1 &&
+      newy >= 1 &&
+      newx <= n &&
+      newy <= n &&
+      map[newx][newy] !== 2
+    ) {
+      if (map[newx][newy] === 0) {
+        const [tailx, taily] = queue.shift();
+        map[tailx][taily] = 0;
+      }
+      queue.push([newx, newy]);
+      map[newx][newy] = 2;
     } else {
-      start = mid + 1;
+      break;
     }
+
+    const found = directions.find(([t, _]) => parseInt(t) === time);
+    if (found) {
+      const [_, route] = found;
+      const value = checkDir(route, dir);
+      dir = value;
+    }
+    x = newx;
+    y = newy;
   }
-  return start;
-};
-const result = targetArr.map(
-  (num) => upperBound(num, findArr) - lowerBound(num, findArr)
-);
-console.log(result.join(" "));
+  return time;
+}
+
+function Solution(map, directions, n) {
+  let time = 0;
+
+  console.log(bfs(map, directions, n, time));
+}
+
+Solution(map, directions, n);
--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -1,23 +1,63 @@
+const { clear } = require("console");
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n] = input.shift().split(" ").map(Number);
-const arr = input.map((v) => v.split(" ").map(Number));
-const startTime = arr.map((v) => v[0]).sort((a, b) => a - b);
-const endTime = arr.map((v) => v[1]).sort((a, b) => a - b);
-
-let start = 0;
-let end = 0;
-let count = 0;
-let result = 0;
-while (start < n) {
-  if (startTime[start] >= endTime[end] + 10) {
-    end++;
-    count--;
-  } else {
-    start++;
-    count++;
-  }
-  result = Math.max(count, count);
+const [n] = input.shift(" ").split(" ").map(Number);
+const [apple] = input.shift().split(" ").map(Number);
+const map = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+for (let i = 0; i < apple; i++) {
+  const [x, y] = input.shift().split(" ").map(Number);
+  map[x][y] = 1;
 }
-console.log(result);
+const [dir] = input.shift().split(" ").map(Number);
+const directions = input.map((v) => v.split(" ").map(String));
+
+const dx = [0, 1, 0, -1];
+const dy = [1, 0, -1, 0];
+function Solution(map, directions, n) {
+  const bfs = (map, directions, n, time) => {
+    let direction = 0;
+    const queue = [[1, 1]];
+    map[1][1] = 2;
+    const checkDir = (dir) => {
+      if (dir == "L") {
+        return (direction + 3) % 4;
+      } else {
+        return (direction + 1) % 4;
+      }
+    };
+    while (queue.length > 0) {
+      const [x, y] = queue.shift();
+      const newx = x + dx[direction];
+      const newy = y + dy[direction];
+      if (
+        newx >= 1 &&
+        newy >= 1 &&
+        newx <= n &&
+        newy <= n &&
+        map[newx][newy] !== 2
+      ) {
+        if (map[newx][newy] === 0) {
+          map[x][y] = 0;
+        }
+        queue.push([newx, newy]);
+        map[newx][newy] = 2;
+      } else {
+        time++;
+        break;
+      }
+      time++;
+      const found = directions.find(([t, _]) => parseInt(t) === time);
+      if (found) {
+        const [_, dir] = found;
+        const value = checkDir(dir);
+        direction = value;
+      }
+    }
+    return time;
+  };
+  let time = 0;
+  console.log(bfs(map, directions, n, time));
+}
+
+Solution(map, directions, n);
