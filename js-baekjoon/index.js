f3c68fa5b6e1fd3bab5976c84e4e7470e775aed2
kwangyoulsagong <sgky0511@naver.com>
2025-03-21 14:39:41 +0900

    feat: 공유기 설치

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -2,12 +2,27 @@ const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
 const [n, m] = input.shift().split(" ").map(Number);
-const arr = input[0].split(" ").map(Number);
-const batteries = Array(m).fill(0);
-arr.sort((a, b) => b - a);
-for (let i = 0; i < n; i++) {
-  batteries.sort((a, b) => a - b);
-  batteries[0] += arr[i];
-}
+const arr = input.map(Number);
+arr.sort((a, b) => a - b);
+let start = 1;
+let end = arr[n - 1] - arr[0];
+let result = 0;
+while (start <= end) {
+  let count = 1;
+  const mid = Math.floor((start + end) / 2);
+  let startHouse = arr[0];
+  for (const house of arr) {
+    if (house - startHouse >= mid) {
+      startHouse = house;
+      count++;
+    }
+  }
 
-console.log(Math.max(...batteries));
+  if (count >= m) {
+    result = Math.max(result, mid);
+    start = mid + 1;
+  } else {
+    end = mid - 1;
+  }
+}
+console.log(result);
