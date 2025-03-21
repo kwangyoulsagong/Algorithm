ffdd27e307b71c807f63729f13e219c1160eef0e
kwangyoulsagong <sgky0511@naver.com>
2025-03-21 13:26:24 +0900

    feat: node /Users/sagong-gwang-yeol/Desktop/Algorithm/js-baekjoon/index.js

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,43 +1,14 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n, k] = input.shift().split(" ").map(Number);
-const map = [];
-const viruses = [];
-for (let i = 0; i < n; i++) {
-  const arr = input.shift().split(" ").map(Number);
-  map.push(arr);
-  for (let j = 0; j < n; j++) {
-    if (arr[j] !== 0) {
-      viruses.push([i, j, arr[j], 0]);
-    }
+const [n] = input.shift().split(" ").map(Number);
+const planet = input[0].split(" ").map(Number);
+let answer = 0;
+for (let i = n - 1; i >= 0; i--) {
+  if (answer <= planet[i]) {
+    answer = planet[i];
+  } else {
+    answer = Math.ceil(answer / planet[i]) * planet[i];
   }
 }
-const [t, x, y] = input.shift().split(" ").map(Number);
-const visited = Array.from({ length: n }, () => Array(k).fill(false));
-
-viruses.sort((a, b) => a[2] - b[2]);
-
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-while (viruses.length > 0) {
-  const [x, y, vir, time] = viruses.shift();
-  if (time == t) break;
-  for (let i = 0; i < 4; i++) {
-    const newx = x + dx[i];
-    const newy = y + dy[i];
-    if (
-      newx >= 0 &&
-      newy >= 0 &&
-      newx < n &&
-      newy < k &&
-      !visited[newx][newy] &&
-      map[newx][newy] === 0
-    ) {
-      map[newx][newy] = vir;
-      viruses.push([newx, newy, vir, time + 1]);
-      visited[newx][newy] = true;
-    }
-  }
-}
-console.log(map[x - 1][y - 1]);
+console.log(answer);
