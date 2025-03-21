06e47e75d3bc97dbf44c9475acf0102fa5154f46
kwangyoulsagong <sgky0511@naver.com>
2025-03-21 12:56:03 +0900

    feat: 경쟁적 전염

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,27 +1,43 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [t] = input.shift().split(" ").map(Number);
-
-const findThePeople = (floor, number) => {
-  const dp = Array.from({ length: floor + 1 }, () => Array(number).fill(0));
-  for (let i = 0; i < number; i++) {
-    dp[0][i] = i + 1;
-  }
-  for (let i = 0; i < floor + 1; i++) {
-    dp[i][0] = 1;
-  }
-  for (let i = 1; i < floor + 1; i++) {
-    for (let j = 1; j < number; j++) {
-      dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+const [n, k] = input.shift().split(" ").map(Number);
+const map = [];
+const viruses = [];
+for (let i = 0; i < n; i++) {
+  const arr = input.shift().split(" ").map(Number);
+  map.push(arr);
+  for (let j = 0; j < n; j++) {
+    if (arr[j] !== 0) {
+      viruses.push([i, j, arr[j], 0]);
     }
   }
-  return dp[floor][number - 1];
-};
-
-for (let i = 0; i < t; i++) {
-  const [floor] = input.shift().split(" ").map(Number);
-  const [number] = input.shift().split(" ").map(Number);
-  const result = findThePeople(floor, number);
-  console.log(result);
 }
+const [t, x, y] = input.shift().split(" ").map(Number);
+const visited = Array.from({ length: n }, () => Array(k).fill(false));
+
+viruses.sort((a, b) => a[2] - b[2]);
+
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+while (viruses.length > 0) {
+  const [x, y, vir, time] = viruses.shift();
+  if (time == t) break;
+  for (let i = 0; i < 4; i++) {
+    const newx = x + dx[i];
+    const newy = y + dy[i];
+    if (
+      newx >= 0 &&
+      newy >= 0 &&
+      newx < n &&
+      newy < k &&
+      !visited[newx][newy] &&
+      map[newx][newy] === 0
+    ) {
+      map[newx][newy] = vir;
+      viruses.push([newx, newy, vir, time + 1]);
+      visited[newx][newy] = true;
+    }
+  }
+}
+console.log(map[x - 1][y - 1]);
