ae55d9c4ef1a4efa74ca368d3991672085ae3971
kwangyoulsagong <sgky0511@naver.com>
2025-03-21 14:23:40 +0900

    feat: 콘센트

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,14 +1,13 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n] = input.shift().split(" ").map(Number);
-const planet = input[0].split(" ").map(Number);
-let answer = 0;
-for (let i = n - 1; i >= 0; i--) {
-  if (answer <= planet[i]) {
-    answer = planet[i];
-  } else {
-    answer = Math.ceil(answer / planet[i]) * planet[i];
-  }
+const [n, m] = input.shift().split(" ").map(Number);
+const arr = input[0].split(" ").map(Number);
+const batteries = Array(m).fill(0);
+arr.sort((a, b) => b - a);
+for (let i = 0; i < n; i++) {
+  batteries.sort((a, b) => a - b);
+  batteries[0] += arr[i];
 }
-console.log(answer);
+
+console.log(Math.max(...batteries));
