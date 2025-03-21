27855c8cebbd0b8eb6de53010b8570b49b633fe0
kwangyoulsagong <sgky0511@naver.com>
2025-03-21 19:24:56 +0900

    fat: 병사 배치하기

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,28 +1,14 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n, m] = input.shift().split(" ").map(Number);
-const arr = input.map(Number);
-arr.sort((a, b) => a - b);
-let start = 1;
-let end = arr[n - 1] - arr[0];
-let result = 0;
-while (start <= end) {
-  let count = 1;
-  const mid = Math.floor((start + end) / 2);
-  let startHouse = arr[0];
-  for (const house of arr) {
-    if (house - startHouse >= mid) {
-      startHouse = house;
-      count++;
+const [n] = input.shift().split(" ").map(Number);
+const arr = input[0].split(" ").map(Number);
+const dp = Array(n).fill(1);
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (arr[i] < arr[j]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
     }
   }
-
-  if (count >= m) {
-    result = Math.max(result, mid);
-    start = mid + 1;
-  } else {
-    end = mid - 1;
-  }
 }
-console.log(result);
+console.log(n - Math.max(...dp));
