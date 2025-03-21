bc7bb08d4f736252c7130d3643fe933586d48a3b
kwangyoulsagong <sgky0511@naver.com>
2025-03-21 11:52:59 +0900

    feat: 부녀회장이될테야

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,33 +1,27 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [game, win] = input[0].split(" ").map(Number);
+const [t] = input.shift().split(" ").map(Number);
 
-function binarySearch(start, end, game, win, percent) {
-  let result = 0;
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    const check = Math.floor((100 * (win + mid)) / (game + mid));
-    if (check > percent) {
-      result = mid;
-      end = mid - 1;
-    } else {
-      start = mid + 1;
+const findThePeople = (floor, number) => {
+  const dp = Array.from({ length: floor + 1 }, () => Array(number).fill(0));
+  for (let i = 0; i < number; i++) {
+    dp[0][i] = i + 1;
+  }
+  for (let i = 0; i < floor + 1; i++) {
+    dp[i][0] = 1;
+  }
+  for (let i = 1; i < floor + 1; i++) {
+    for (let j = 1; j < number; j++) {
+      dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
     }
   }
-  return result;
-}
+  return dp[floor][number - 1];
+};
 
-function Solution(game, win) {
-  const percent = Math.floor((100 * win) / game);
-  let start = 0;
-  let end = game;
-  if (percent >= 99) {
-    console.log(-1);
-  } else {
-    const answer = binarySearch(start, end, game, win, percent);
-    console.log(answer);
-  }
+for (let i = 0; i < t; i++) {
+  const [floor] = input.shift().split(" ").map(Number);
+  const [number] = input.shift().split(" ").map(Number);
+  const result = findThePeople(floor, number);
+  console.log(result);
 }
-
-Solution(game, win);
