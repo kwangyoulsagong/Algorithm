b3aad08a9b35ba9074505f92e4b3892cbe37aced
kwangyoulsagong <sgky0511@naver.com>
2025-04-03 13:14:28 +0900

    feat: 팩토리얼0의개수

--- js-baekjoon/tempCodeRunnerFile.js
+++ js-baekjoon/tempCodeRunnerFile.js
@@ -1,20 +1,11 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-const [n] = input.shift().split(" ").map(Number);
-const findTheVPS = (arr) => {
-  let flag = "NO";
-  let left = 0;
-  let right = 0;
-  for (const value of arr) {
-    if (value === "(") left++;
-    else right++;
-  }
-  if (left === right) flag = "YES";
-  return flag;
-};
-for (let i = 0; i < n; i++) {
-  const arr = input[i].split("").map(String);
-  const result = findTheVPS(arr);
-  console.log(result);
+let [n] = input.shift().split(" ").map(Number);
+
+let count = 0;
+while (n >= 5) {
+  count += n / 5;
+  n /= 5;
 }
+console.log(count);
