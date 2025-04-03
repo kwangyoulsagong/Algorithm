bf1a0d3b2cd652e93b6d05777f0564f69952d5f9
kwangyoulsagong <sgky0511@naver.com>
2025-04-03 12:13:27 +0900

    feat: 최대공약수 와 최소공배수

--- js-baekjoon/bronzeI/2609최대공약수와최소공배수.js
+++ js-baekjoon/bronzeI/2609최대공약수와최소공배수.js
@@ -2,10 +2,14 @@ const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
 let [a, b] = input.shift().split(" ").map(Number);
-
-const GCD = (a, b) => {
-  if (b === 0) return a;
-  return GCD(b, a % b);
-};
-console.log(GCD(a, b));
-console.log((a * b) / GCD(a, b));
+let A = a,
+  B = b;
+while (a % b !== 0) {
+  let n = a % b;
+  if (n != 0) {
+    a = b;
+    b = n;
+  }
+}
+console.log(b);
+console.log((A * B) / b);
