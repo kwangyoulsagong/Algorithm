1968ea07c2db4091010581d128f7d1248b12eebe
kwangyoulsagong <sgky0511@naver.com>
2025-04-08 19:27:15 +0900

    feat: 구간합구하기4

--- js-baekjoon/index.js
+++ js-baekjoon/index.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-let [n] = input.shift().split(" ").map(Number);
-const count123Addition = (value) => {
-  const dp = Array(value + 1).fill(0);
-  dp[1] = 1;
-  dp[2] = 2;
-  dp[3] = 4;
-  for (let i = 4; i < value + 1; i++) {
-    dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
-  }
-  return dp[value];
+let [n, m] = input.shift().split(" ").map(Number);
+const arr = input.shift().split(" ").map(Number);
+for (let i = 1; i < n; i++) {
+  arr[i] += arr[i - 1];
+}
+
+const calculatePrefixSum = (start, end) => {
+  if (start - 2 === -1) return arr[end - 1];
+  return arr[end - 1] - arr[start - 2];
 };
-for (let i = 0; i < n; i++) {
-  const [value] = input[i].split(" ").map(Number);
-  const result = count123Addition(value);
+
+for (let i = 0; i < m; i++) {
+  const [start, end] = input[i].split(" ").map(Number);
+  const result = calculatePrefixSum(start, end);
   console.log(result);
 }
